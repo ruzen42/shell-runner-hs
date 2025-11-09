74114e493208c1cd2276225f15a5feaa6fa894ad
ruzen42- <malledeferlive@gmail.com>
2025-11-09 18:42:39 +0500

    feat(init usable)

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,40 +1,63 @@
 {-# LANGUAGE DataKinds       #-}
-{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeOperators   #-}
-module Lib
-    ( startApp
-    , app
-    ) where
+{-# LANGUAGE DeriveGeneric   #-}
+{-# LANGUAGE OverloadedStrings #-}
 
-import Data.Aeson
-import Data.Aeson.TH
+module Lib (startApp, app) where
+
+import GHC.Generics
+import Servant
 import Network.Wai
 import Network.Wai.Handler.Warp
-import Servant
+import Data.Text (Text, pack)
+import qualified Data.Text.IO as T
+import Data.Aeson
+import System.Process
+import System.Exit
+import Control.Exception (try, SomeException)
 
-data User = User
-  { userId        :: Int
-  , userFirstName :: String
-  , userLastName  :: String
-  } deriving (Eq, Show)
+data CommandRequest = CommandRequest
+  { command :: String
+  } deriving (Eq, Show, Generic)
 
-$(deriveJSON defaultOptions ''User)
+instance FromJSON CommandRequest
 
-type API = "users" :> Get '[JSON] [User]
+data CommandResponse = CommandResponse
+  { output   :: Maybe Text
+  , err      :: Maybe Text
+  , exitCode :: Int
+  } deriving (Eq, Show, Generic)
 
-startApp :: IO ()
-startApp = run 8080 app
+instance ToJSON CommandResponse
 
-app :: Application
-app = serve api server
+type API =
+       "run" :> ReqBody '[JSON] CommandRequest
+             :> Post '[JSON] CommandResponse
+
+server :: Server API
+server = runCommand
+
+runCommand :: CommandRequest -> Handler CommandResponse
+runCommand (CommandRequest cmd) = do
+  result <- liftIO $ try (readCreateProcessWithExitCode (shell cmd) "") :: IO (Either SomeException (ExitCode, String, String))
+  case result of
+    Left ex -> pure $ CommandResponse Nothing (Just $ pack $ show ex) 1
+    Right (code, out, err') ->
+      pure $ CommandResponse
+        { output = if null out then Nothing else Just $ pack out
+        , err = if null err' then Nothing else Just $ pack err'
+        , exitCode = case code of
+            ExitSuccess   -> 0
+            ExitFailure n -> n
+        }
 
 api :: Proxy API
 api = Proxy
 
-server :: Server API
-server = return users
+app :: Application
+app = serve api server
 
-users :: [User]
-users = [ User 1 "Isaac" "Newton"
-        , User 2 "Albert" "Einstein"
-        ]
+startApp :: Int -> IO ()
+startApp port = do
+  putStrLn "🚀 Running on http://localhost:" ++ show port
+  run port app
