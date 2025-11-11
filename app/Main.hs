e409db703c2bae17f0edc2d1d35e0d98996897ab
ruzen42- <malledeferlive@gmail.com>
2025-11-11 22:31:48 +0500

    fix(safer read int)

--- app/Main.hs
+++ app/Main.hs
@@ -3,12 +3,13 @@ module Main (main) where
 import Lib
 import GHC.Conc (getNumProcessors)
 import System.Environment (getArgs)
+import Text.Read (readMaybe)
 
 main :: IO ()
 main = do
     n <- getNumProcessors
     args <- getArgs
     let port = case args of
-            (x:_) -> read x :: Int
+            (x:_) -> maybe 8080 id (readMaybe x :: Maybe Int)
             []    -> 8080
     startApp port n
